e4052fc01919b29a84be81a5393cc8aba2592d52
alberto <amouraammarabderafik@gmail.com>
2023-03-03 20:10:42 +0300

    create new failure function

--- fstructure.sh
+++ fstructure.sh
@@ -127,12 +127,30 @@ function __create_normal_screen() {
 		__error
 }
 
+# _create_new_failure .........................................
+
+function __create_new_failure() {
+	if [ "$#" -gt 1 ]; then
+		if [ -d "lib/core/errors" ]; then
+			i=2
+			while [ "$i" -le "$#" ]; do
+				value=$(eval "echo \"\${$i}\"")
+				__create_error $value
+				i=$((i + 1))
+			done
+		else
+			echo "You need init the project first"
+		fi
+	else
+		echo "ERROR: Invalid command or incorrect number of arguments."
+	fi
+}
 # _create_error .................................................
 
-function __create_error(){
+function __create_error() {
 	cd "$r/lib/core/errors" &&
-	echo "\n$(__change_content errors/new_error.dart _ $1)\n" >>exceptions.dart &&
-	echo "\n$(__change_content errors/new_failure.dart _ $1)\n" >>failures.dart ||
+		echo "\n$(__change_content errors/new_error.dart _ $1)\n" >>exceptions.dart &&
+		echo "\n$(__change_content errors/new_failure.dart _ $1)\n" >>failures.dart ||
 		echo "something went wrong ..."
 }
 # project structure --------------------------#
@@ -246,6 +264,9 @@ function fs() {
 				echo 'done'
 		fi
 		;;
+	'-cf')
+		__create_new_failure $@
+		;;
 	'--help')
 		__help
 		;;
@@ -319,22 +340,6 @@ function fsm() {
 	esac
 }
 
-function fsge() {
-	if [ "$#" -gt 0 ]; then
-			if [ -d "lib/core/errors" ]; then
-				i=1
-				while [ "$i" -le "$#" ]; do
-					value=$(eval "echo \"\${$i}\"")
-					__create_error $value
-					i=$((i+1))
-				done
-			else
-				echo "You need init the project first"
-			fi
-		else
-			echo "ERROR: Invalid command or incorrect number of arguments."
-		fi
-}
 function __help() {
 	echo "Usage: fs [command] [arguments]"
 	echo ""
